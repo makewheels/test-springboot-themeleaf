6f5e2286f8088244d1b477861b7490559c24da09
makewheels <makewheels>
2021-04-03 15:37:43 +0800

    操作和，头像

--- src/main/java/com/example/testthemeleaf/RouteController.java
+++ src/main/java/com/example/testthemeleaf/RouteController.java
@@ -1,8 +1,15 @@
 package com.example.testthemeleaf;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping("/page")
@@ -14,7 +21,30 @@ public class RouteController {
     }
 
     @GetMapping("/user")
-    public String user() {
+    public String user(ModelMap modelMap) {
+        String logo = "https://postimgs.org/img/bookshelf.jpg";
+        User u1 = new User(1, "ffawef", logo, new Date());
+        User u2 = new User(2, "34t3g4r", logo, new Date());
+        User u3 = new User(3, "g34f34fawe", logo, new Date());
+        List<User> userList = new ArrayList<>();
+        userList.add(u1);
+        userList.add(u2);
+        userList.add(u3);
+        modelMap.put("userList", userList);
         return "page/user";
+    }
+
+    @GetMapping("/deleteUser")
+    public String deleteUser(@RequestParam Integer id, @RequestParam String name) {
+        System.out.println("DELETE USER: id = " + id);
+        System.out.println("DELETE USER: name = " + name);
+        return "redirect:user";
+    }
+
+    @GetMapping("/updateUser")
+    public String updateUser(@RequestParam Integer id, @RequestParam String name) {
+        System.out.println("UPDATE USER: id = " + id);
+        System.out.println("UPDATE USER: name = " + name);
+        return "redirect:user";
     }
 }
--- src/main/java/com/example/testthemeleaf/User.java
+++ src/main/java/com/example/testthemeleaf/User.java
@@ -1,12 +1,17 @@
 package com.example.testthemeleaf;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private Integer id;
     private String name;
-    private Integer age;
+    private String logo;
     private Date createTime;
 }
